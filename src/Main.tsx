6edceade7efe1be03226717da575b9d4b6e70d14
Adhim Nioka <niokagi@gmail.com>
2023-11-09 23:30:05 +0700

    third

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,15 +1,21 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./style/style.css";
 import { App } from "./App.tsx";
+import "./style/style.css";
 
 const rootElement = document.getElementById("root")!;
 
-const renderWithStrictMode = (element: any, component: any) => {
-  ReactDOM.createRoot(element).render(
-    <React.StrictMode>{component}</React.StrictMode>
-  );
-};
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-renderWithStrictMode(rootElement, <App />);
+// const renderWithStrictMode = (element: any, component: any) => {
+//   ReactDOM.createRoot(element).render(
+//     <React.StrictMode>{component}</React.StrictMode>
+//   );
+// };
+
+// renderWithStrictMode(rootElement, <App />);
