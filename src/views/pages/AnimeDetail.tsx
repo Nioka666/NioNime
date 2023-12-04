634d69cce3c20fc06336979c7255baf880a38579
Adhim Nioka <niokagi@gmail.com>
2023-12-04 16:30:30 +0700

    last 4 Des

--- src/views/pages/AnimeDetail.tsx
+++ src/views/pages/AnimeDetail.tsx
@@ -27,7 +27,7 @@ export const AnimeDetail = () => {
           backgroundImage: bannerImage ? `url(${bannerImage})` : "none",
           backgroundSize: "cover",
           filter: "brightness(38%)",
-          height: "520px",
+          height: "550px",
           zIndex: "-999",
           cursor: "pointer",
           overflow: "hidden",
@@ -41,66 +41,125 @@ export const AnimeDetail = () => {
             width: "100%",
             height: "150px",
             background:
-              "linear-gradient(to bottom, rgba(0,0,0,0), rgba(0,0,0,0.8))",
+              "linear-gradient(to bottom, rgba(0,0,0,0), rgba(0, 0, 0, 0.689))",
           }}
         ></div>
       </div>
 
       <div
         className="container profile-page"
-        style={{ margin: "-315px 0 0 72px" }}
+        style={{ margin: "-345px 0 0 72px" }}
       >
-        <div className="row d-flex">
-          <div className="col-lg-4 d-flex w-100 user-wrapper">
-            <img
-              src={animeDetail?.coverImage}
-              height={"317px"}
-              width={"227px"}
-              className="bd-placeholder-img rounded-1"
+        {/* <div className="row d-flex"> */}
+        <div className="col-lg-4 d-flex w-100 user-wrapper">
+          <img
+            src={animeDetail?.coverImage}
+            width={"230px"}
+            className="bd-placeholder-img rounded-1"
+            style={{
+              border: "0px solid black",
+              cursor: "pointer",
+              marginTop: "-80px",
+            }}
+          />
+          <div
+            className="anime-detail-content"
+            style={{
+              display: "grid",
+              height: "50px",
+              marginLeft: "30px",
+              gap: "10px",
+            }}
+          >
+            <h4
+              className="anime-detail-title"
               style={{
-                border: "0px solid black",
-                cursor: "pointer",
-                marginTop: "-80px",
-              }}
-            />
-            <div
-              className="anime-detail-content"
-              style={{
-                display: "grid",
-                height: "50px",
-                marginLeft: "25px",
-                gap: "5px",
+                margin: "-65px 0 0 0px",
+                fontWeight: "500",
+                fontSize: "42px",
+                width: "80%",
               }}
             >
-              <h4
-                className="anime-detail-title"
+              {animeDetail?.title.romaji} <br />
+            </h4>
+            <div
+              className="status-badge"
+              style={{
+                display: "flex",
+                gap: "10px",
+                height: "29px",
+              }}
+            >
+              <span className="anime-detail-span">{animeDetail?.status}</span>
+              <span className="anime-detail-span">{animeDetail?.season}</span>
+              <span
+                className="anime-detail-span"
+                style={{ color: "yellow", border: "1px solid yellow" }}
+              >
+                <i
+                  className="fa-solid fa-star"
+                  style={{
+                    fontSize: "14px",
+                    marginRight: "3.5px",
+                    fontWeight: "bold",
+                  }}
+                ></i>
+                {animeDetail?.rating.anilist}
+              </span>
+            </div>
+            <div className="btn-groups mt-2">
+              <button
+                id="btn-banner"
+                className="btn btn-lg text-white"
+                type="button"
                 style={{
-                  margin: "-65px 0 0 0px",
-                  fontWeight: "500",
-                  fontSize: "42px",
-                  width: "80%",
+                  border: "2px solid gray",
+                  width: "215px",
+                  padding: "10px 10px",
+                  fontSize: "17px",
                 }}
               >
-                {animeDetail?.title.romaji} <br />
-              </h4>
-              <div
-                className="status-badge"
+                <i
+                  className="fa-solid fa-plus"
+                  style={{ marginRight: "8px" }}
+                ></i>
+                Add to Watchlist
+              </button>
+
+              <button
+                id="btn-banner"
+                className="btn btn-lg text-white"
+                type="button"
                 style={{
-                  display: "flex",
-                  gap: "10px",
-                  height: "29px",
+                  border: "2px solid #caa800",
+                  backgroundColor: "#caa800",
+                  width: "165px",
+                  padding: "10px 10px",
+                  fontSize: "17px",
+                  marginLeft: "10px",
                 }}
               >
-                <span className="anime-detail-span">{animeDetail?.status}</span>
-                <span className="anime-detail-span">{animeDetail?.season}</span>
-              </div>
-              <div className="description mt-4" style={{ width: "80%" }}>
-                <p style={{ fontSize: "15px" }}>
-                  {animeDetail?.description.substring(0, 300)}
-                </p>
-              </div>
+                <i
+                  className="fa-solid fa-plus"
+                  style={{ marginRight: "8px" }}
+                ></i>
+                Watch Now
+              </button>
+            </div>
+            <div className="description mt-4" style={{ width: "80%" }}>
+              <p style={{ fontSize: "15px" }}>
+                {animeDetail?.description.substring(0, 300)}
+              </p>
             </div>
           </div>
+        </div>
+        {/* </div> */}
+        <div className="div">
+          <br />
+          <br />
+          <br />
+          <br />
+          <h1>test</h1>
         </div>
       </div>
     </>
